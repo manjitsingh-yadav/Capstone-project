62e2a56ff71118d9ab44677c75713a0f9072e494
manjitsingh-yadav <tcsmanjit@gmail.com>
2022-03-22 12:24:17 +0000

    k8s installation scripts

---
+++ install-k8s-worker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+  
+
+# 1. Install docker
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo echo -e "{\n\t\"exec-opts\": [\"native.cgroupdriver=systemd\"]\n}" > sudo /etc/docker/daemon.json
+
+sudo service docker restart
+
+#sudo service docker status 
+
+docker info | grep -i cgroup
+
+# 2. Install Kubernetes components
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt-get update ; clear
+
+sudo apt-get install -y kubelet kubeadm
