a7b30ee5a01a6632471bebf4d268e343b39ccfd3
manjitsingh-yadav <tcsmanjit@gmail.com>
2022-03-23 13:05:59 +0000

    Another commit

--- install-k8s-master.sh
+++ install-k8s-master.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-  
 
 # 1. Install docker
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y docker-ce
+apt-get install -y docker-ce
 
 echo -e "{\n\t\"exec-opts\": [\"native.cgroupdriver=systemd\"]\n}" | sudo tee /etc/docker/daemon.json
 
-sudo service docker restart
-
-#sudo service docker status 
-
-docker info | grep -i cgroup
+service docker restart
 
 # 2. Install Kubernetes components
 
@@ -29,18 +24,10 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet kubeadm kubectl
 
-sudo kubeadm init
+kubeadm init
 
-mkdir -p $HOME/.kube
-
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# 3. Install networking solution (Weave)
-
-sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(sudo kubectl version | base64 | tr -d '\n')"
+touch done.txt
--- install-k8s-worker.sh
+++ install-k8s-worker.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-  
 
 # 1. Install docker
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y docker-ce
+apt-get install -y docker-ce
 
 echo -e "{\n\t\"exec-opts\": [\"native.cgroupdriver=systemd\"]\n}" | sudo tee /etc/docker/daemon.json
 
-sudo service docker restart
-
-#sudo service docker status 
-
-docker info | grep -i cgroup
+systemctl restart docker
 
 # 2. Install Kubernetes components
 
@@ -29,6 +24,8 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y kubelet kubeadm
+apt-get install -y kubelet kubeadm
+
+touch done.txt
